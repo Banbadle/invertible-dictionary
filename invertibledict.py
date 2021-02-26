9df7d06ebdc149320c40639bf28a5418d88688ac
Banbadle <78810400+Banbadle@users.noreply.github.com>
2021-02-26 01:59:36 +0000

    Add files via upload

--- invertibledict.py
+++ invertibledict.py
@@ -11,6 +11,53 @@ class InvDict():
             for key, val in dic.getItems():
                 dic[key] = val
             
+    def clear(self):
+        self.dict.clear()
+        self.inverse.clear()   
+        
+    def copy(self):
+        dic = self.dict().copy()
+        inv = self.inverse().copy()
+        
+        newInvDict          = InvDict()
+        newInvDict.dict     = dic
+        newInvDict.inverse  = inv
+        
+        return newInvDict
+        
+    def fromkeys(self):
+        pass
+    
+    def get(self, key):
+        return self.dict.get(key)
+    
+    def items(self):
+        return self.dict.items()
+    
+    def keys(self):
+        return self.dict.items()
+    
+    def pop(self, key):
+        
+        val = self.dict.pop(key)
+        self.getInv(val).pop(key)
+        
+        if len(self.getInv(val))  == 0 :
+            self.inverse.pop(val)
+        return val
+            
+    def popitem(self):
+        pass
+    
+    def setdefault(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def values(self):
+        return self.dict.values()
+    
     def getInv(self, val):
         return self.inverse[val]
         
@@ -35,6 +82,9 @@ class InvDict():
             self.dict[key] = val
         
         #If balue is not new
+        #
+        #
+        #May be better as a tuple
         if val in self.inverse:
             self.getInv(val).append(key)
             
@@ -44,16 +94,8 @@ class InvDict():
 
     def __getitem__(self, key):
         return self.dict[key]
-    
-    def pop(self, key):
-        
-        val = self.dict.pop(key)
-        self.getInv(val).pop(key)
-        
-        if len(self.getInv(val))  == 0 :
-            self.inverse.pop(val)
-        return val
-    
+
+
     def __str__(self):
         return self.dict.__str__()
         
@@ -69,3 +111,5 @@ print(test)
 print(test[1])
 print(test.pop(1))
 print(test.getInv(24))
+
+print(test.get(2))
