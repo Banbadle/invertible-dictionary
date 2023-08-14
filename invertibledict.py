a4974612c0ab3bd3078b7bb4d7fdf7ce7f66f97f
Banbadle <archiealmond@hotmail.co.uk>
2023-08-14 01:32:51 +0100

    Underscored dictionary variables
    Should not access these dictionaries directly

--- invertibledict.py
+++ invertibledict.py
@@ -1,29 +1,32 @@
-# Invertible dictionary
-# Can be used as a regular dictionary
-# Additional getInv(val) method, which returns a tuple of all keys which have a value of val
+
 class InvDict():
-    
+    '''
+    Invertible dictionary
+    Can be used as a regular dictionary
+    Additional getinv(val) method: 
+        - Returns a tuple of all keys which have a value of val
+    '''
     def __init__(self, iterable=None):
-        self.dict       = dict()
-        self.inverse    = dict()
+        self._dict       = dict()
+        self._inverse    = dict()
         
         if iterable == None:
             return
             
         for key, val in iterable:
-                self[key] = val
+            self[key] = val
             
     def clear(self):
-        self.dict.clear()
-        self.inverse.clear()   
+        self._dict.clear()
+        self._inverse.clear()   
         
     def copy(self):
-        dic = self.dict().copy()
-        inv = self.inverse().copy()
+        dic = self._dict().copy()
+        inv = self._inverse().copy()
         
         newInvDict          = InvDict()
-        newInvDict.dict     = dic
-        newInvDict.inverse  = inv
+        newInvDict._dict    = dic
+        newInvDict._inverse = inv
         
         return newInvDict
         
@@ -32,31 +35,31 @@ class InvDict():
             self[key] = val
     
     def get(self, key):
-        return self.dict.get(key)
+        return self._dict.get(key)
     
     def items(self):
-        return self.dict.items()
+        return self._dict.items()
     
     def keys(self):
-        return self.dict.items()
+        return self._dict.items()
     
     def pop(self, key):
         
-        val = self.dict.pop(key)
-        self.inverse[val].pop(key)
+        val = self._dict.pop(key)
+        self._inverse[val].pop(key)
         
-        if len(self.getInv(val))  == 0 :
-            self.inverse.pop(val)
+        if len(self.getinv(val))  == 0 :
+            self._inverse.pop(val)
         return val
             
     def popitem(self):
-        key, val = self.dict.popitem()
-        self.inverse[val].remove(key)
+        key, val = self._dict.popitem()
+        self._inverse[val].remove(key)
             
         return val;
     
     def setdefault(self, key, defaultValue=None):
-        if key not in self.dict:
+        if key not in self._dict:
             self[key] = defaultValue
             
     def update(self, iterable):
@@ -69,30 +72,32 @@ class InvDict():
                 self[key] = val
     
     def values(self):
-        return self.dict.values()
+        return self._dict.values()
     
-    #Returns a tuple of all keys which have a value of val
-    def getInv(self, val):
-        return tuple(self.inverse[val])
+    def getinv(self, val):
+        '''
+        Returns a tuple of all keys which have a value of 'val'
+        '''
+        return tuple(self._inverse[val])
         
     def __setitem__(self, key, val):
         
         #If key is not new
-        if key in self.dict:
+        if key in self._dict:
             
-            oldVal = self.dict[key]
+            oldVal = self._dict[key]
             if oldVal == val:
                 return
             
-            self.dict[key] = val        
-            self.inverse[oldVal].remove(key)
+            self._dict[key] = val        
+            self._inverse[oldVal].remove(key)
             
-            if len(self.getInv(oldVal)) == 0:
-                self.inverse.pop(oldVal)
+            if len(self.getinv(oldVal)) == 0:
+                self._inverse.pop(oldVal)
                 
         #If key is new
         else:
-            self.dict[key] = val
+            self._dict[key] = val
 
         #If value is not new
         if val in self.inverse:
@@ -103,11 +108,10 @@ class InvDict():
             self.inverse[val] = [key]
             
     def __iter__(self):
-        return self.dict.__iter__()
+        return self._dict.__iter__()
 
     def __getitem__(self, key):
-        return self.dict[key]
-
+        return self._dict[key]
 
     def __str__(self):
-        return self.dict.__str__()
+        return self._dict.__str__()
